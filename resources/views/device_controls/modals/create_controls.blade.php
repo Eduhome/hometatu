3f91f8cd28c8988df4c766fa20303e6647a0c498
Eduhome <erjusti23@gmail.com>
2024-08-26 00:40:14 -0400

    Configurabndo a produccion

--- resources/views/device_controls/modals/create_controls.blade.php
+++ resources/views/device_controls/modals/create_controls.blade.php
@@ -6,6 +6,7 @@
         <h5 class="modal-title" id="modalCenterTitle">Crear variable</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
+      <h1>erikc</h1>
       <div class="modal-body">
         <form id="deviceControlForm">
           @csrf
